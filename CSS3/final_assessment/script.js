be1eb7c26803545c56fb8e35b55745f2123f20cb
Alokananda Y <plantsfortrees@gmail.com>
2024-11-19 03:05:30 +0000

    Adding some JS

---
+++ CSS3/final_assessment/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+	// Get the Span element
+	const dateSpanEl = document.getElementById('date');
+    // Get the current year
+	const currentYear = new Date().getFullYear();
+	console.log(currentYear);
+
+});
